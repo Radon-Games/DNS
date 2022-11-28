aa019acc823527adbe3b1665a7f5a43fe940bf9e
Cohen Erickson <cohenerickson@gmail.com>
2022-11-28 10:45:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { wire, DNSServer } = require("bns");
-require("dotenv").config();
 
 const server = new DNSServer({ tcp: true, maxConnections: 20, edns: true, ednsSize: 4096, dnssec: true });
-
-const IP = process.env.IP;
 
 const blocked = require("./blocked.js");
 
@@ -16,11 +13,11 @@ server.on("query", (req, res, rinfo) => {
       rr.type = wire.types.A;
       rr.ttl = 3600;
       rr.data = new wire.ARecord();
-      rr.data.address = "135.181.194.21";
+      rr.data.address = "45.89.198.7";
       res.answer.push(rr);
       return res.send();
     }
   });
 });
 
-server.bind(53, IP);
+server.bind(53, "0.0.0.0");
